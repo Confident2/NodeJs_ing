79ea480291a075ffe14e016a18bcaa5da61fde66
mike <46815141+Confident2@users.noreply.github.com>
2023-12-01 16:12:43 +0100

    add router info update

--- server.js
+++ server.js
@@ -35,48 +35,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // serve static files
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/subdir", express.static(path.join(__dirname, "/public")));
 
-app.get("^/$|/index(.html)?", (req, res) => {
-  //res.sendFile("./views/index.html", { root: __dirname });
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html"); //302 by default
-});
-
-//Route Handlers || middlewares
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("first attempt on hello.html");
-    next();
-  },
-  (req, res, next) => {
-    res.send("hello world");
-  }
-);
-
-// chaining route handlers
-const one = (req, res, next) => {
-  console.log("one");
-  next();
-};
-
-const two = (req, res, next) => {
-  console.log("two");
-  next();
-};
-
-const three = (req, res) => {
-  console.log("three");
-  res.send("finished");
-};
-
-app.get("/chain(.html)?", [one, two, three]);
+app.use("/", require("./routes/root"));
+app.use("/subdir", require("./routes/subdir"));
 
 // mostly for middleware and does not use regex
 // app.get("/*", (req, res) => {
